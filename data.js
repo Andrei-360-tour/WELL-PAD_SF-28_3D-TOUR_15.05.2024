b2ba889a1ec3e716470689226768bca7fe65ef85
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-10 14:37:08 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,477 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-28_overview",
+      "name": "SF-28_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.7818833190611478,
+        "pitch": 0.8543226895416431,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.003243885363018,
+          "pitch": 0.34925435983676323,
+          "rotation": 0,
+          "target": "1-sf-28_overview-1"
+        },
+        {
+          "yaw": -1.2366484683560124,
+          "pitch": 0.5341968020044234,
+          "rotation": 0,
+          "target": "3-sf-28_wellhead--pipeline"
+        },
+        {
+          "yaw": -0.8800895600306298,
+          "pitch": 0.5672412556427862,
+          "rotation": 0,
+          "target": "2-sf-28_wellhead"
+        },
+        {
+          "yaw": -1.6168357155842639,
+          "pitch": 0.48859108511505767,
+          "rotation": 0,
+          "target": "5-sf-28_sucker-rod-pumping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-28_overview-1",
+      "name": "SF-28_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.787812430226591,
+        "pitch": 0.7508705913661942,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2222624933820363,
+          "pitch": -0.15853053200881284,
+          "rotation": 0,
+          "target": "0-sf-28_overview"
+        },
+        {
+          "yaw": -2.4748656403379314,
+          "pitch": 0.862199524112448,
+          "rotation": 0,
+          "target": "2-sf-28_wellhead"
+        },
+        {
+          "yaw": -2.038582422790185,
+          "pitch": 0.30357605116840247,
+          "rotation": 0,
+          "target": "7-sf-28_old-security-building--access-road"
+        },
+        {
+          "yaw": 2.237825719911698,
+          "pitch": 0.4943232942613385,
+          "rotation": 0,
+          "target": "5-sf-28_sucker-rod-pumping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-28_wellhead",
+      "name": "SF-28_Wellhead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.10621515175728291,
+        "pitch": 0.15281484875650797,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21437075992386845,
+          "pitch": -0.9908033748608762,
+          "rotation": 0,
+          "target": "1-sf-28_overview-1"
+        },
+        {
+          "yaw": -0.2677041774197608,
+          "pitch": 0.2864396166843406,
+          "rotation": 0,
+          "target": "3-sf-28_wellhead--pipeline"
+        },
+        {
+          "yaw": 3.095351099632552,
+          "pitch": 0.1849373256579181,
+          "rotation": 0,
+          "target": "7-sf-28_old-security-building--access-road"
+        },
+        {
+          "yaw": -1.3552796651750754,
+          "pitch": -0.3846295976789911,
+          "rotation": 0,
+          "target": "0-sf-28_overview"
+        },
+        {
+          "yaw": 1.2766241985362434,
+          "pitch": 0.14099091241148187,
+          "rotation": 0,
+          "target": "4-sf-28_concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-28_wellhead--pipeline",
+      "name": "SF-28_Wellhead & Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.39004322720122886,
+        "pitch": 0.679537889816693,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8359823244675475,
+          "pitch": 0.1868660858964546,
+          "rotation": 0,
+          "target": "2-sf-28_wellhead"
+        },
+        {
+          "yaw": 3.0832773876074366,
+          "pitch": 0.2225089509879652,
+          "rotation": 0,
+          "target": "4-sf-28_concrete-pad"
+        },
+        {
+          "yaw": 1.7513875352525696,
+          "pitch": 0.12449318062856207,
+          "rotation": 0,
+          "target": "5-sf-28_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.22614300791319408,
+          "pitch": -0.6188459345233639,
+          "rotation": 0,
+          "target": "0-sf-28_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-28_concrete-pad",
+      "name": "SF-28_Concrete pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.013488586041777495,
+        "pitch": -0.0648274627213663,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5907792490721402,
+          "pitch": 0.14738390323874562,
+          "rotation": 0,
+          "target": "3-sf-28_wellhead--pipeline"
+        },
+        {
+          "yaw": 2.2458365573625034,
+          "pitch": 0.229229604885008,
+          "rotation": 0,
+          "target": "5-sf-28_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -1.5891146580505975,
+          "pitch": 0.12281550212560433,
+          "rotation": 0,
+          "target": "6-sf-28_well-pad"
+        },
+        {
+          "yaw": 0.1661312864941742,
+          "pitch": 0.06041775876088451,
+          "rotation": 0,
+          "target": "2-sf-28_wellhead"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-28_sucker-rod-pumping-system",
+      "name": "SF-28_Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.3243664758390956,
+        "pitch": 0.04283194763387499,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0275437719159406,
+          "pitch": 0.042263266087859463,
+          "rotation": 0,
+          "target": "6-sf-28_well-pad"
+        },
+        {
+          "yaw": -0.9509517844020436,
+          "pitch": 0.09310052384784306,
+          "rotation": 0,
+          "target": "3-sf-28_wellhead--pipeline"
+        },
+        {
+          "yaw": -0.8583777175618401,
+          "pitch": -0.2263637997029182,
+          "rotation": 0,
+          "target": "1-sf-28_overview-1"
+        },
+        {
+          "yaw": -0.0036814228174453234,
+          "pitch": -0.4833921856251209,
+          "rotation": 0,
+          "target": "0-sf-28_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-28_well-pad",
+      "name": "SF-28_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.5910949877157403,
+        "pitch": -0.2427205689492773,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.514329889117038,
+          "pitch": 0.02140549807690384,
+          "rotation": 0,
+          "target": "5-sf-28_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.9278094438890001,
+          "pitch": 0.009756502250890975,
+          "rotation": 0,
+          "target": "7-sf-28_old-security-building--access-road"
+        },
+        {
+          "yaw": 2.258835353282601,
+          "pitch": 0.08430009396381521,
+          "rotation": 0,
+          "target": "4-sf-28_concrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-28_old-security-building--access-road",
+      "name": "SF-28_Old Security building & Access road",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.060772877824604166,
+        "pitch": 0.24036209981948709,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9961443954917346,
+          "pitch": 0.18750863973173715,
+          "rotation": 0,
+          "target": "2-sf-28_wellhead"
+        },
+        {
+          "yaw": 1.8865964797821695,
+          "pitch": -0.401535373687679,
+          "rotation": 0,
+          "target": "1-sf-28_overview-1"
+        },
+        {
+          "yaw": -2.9067724380973097,
+          "pitch": 0.17134597552938224,
+          "rotation": 0,
+          "target": "6-sf-28_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-28",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
